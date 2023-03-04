a980de3da10be3ef91ec6e62309b9ab88722219f
Andries George Șerban <george.andries@student.usv.ro>
2023-03-04 16:22:15 +0200

    form_delete

--- api/forms/form_router.py
+++ api/forms/form_router.py
@@ -2,8 +2,11 @@
 --Careful with forms here, how to save the fields they have and all.
 - PUT update a form
 - GET all forms of a user"""
-from fastapi import APIRouter
+from fastapi import APIRouter, Path, Depends, HTTPException, Depends, status
 from ..database.cosmo_db import forms_container
+from ..authentication.encryption import get_current_user
+from ..users.models import User
+import azure
 
 router = APIRouter(
     prefix="/api/v1/users/{user_id}/forms"
@@ -49,9 +52,26 @@ async def get_form_data(
 
 @router.delete(path="/{form_id}",
                tags=['forms'])
-async def create_new_form(
-        user_id: str,
-        form_id: str,
-):
-    pass
-    #asdasdsdadasdasd
+async def delete_form(
+        user_id: str = Path(example="Vizitiu Valentin",
+                            description="The email or name of the user."),
+        current_user: User = Depends(get_current_user),
+        form_id: str = Path(example="Vizitiu Valentin",
+                            description="The email or name of the user.")
+
+
+) -> None:
+
+    if current_user.id != user_id:
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="You can delete only your own data.")
+    try:
+        forms_container.delete_item(
+            item = form_id,
+            partition_key=form_id,
+
+        )
+    except azure.cosmos.exceptions.CosmosResourceNotFoundError:#type:ignore
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f"Form ''{form_id}'' does not exist.")
+
+
